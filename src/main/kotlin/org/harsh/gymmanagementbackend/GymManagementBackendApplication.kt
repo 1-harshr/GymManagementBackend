a7cbb8eac7623c689f86b3d41d7f28083f67191e
1-harshr <rharsh.iitk@gmail.com>
2026-06-11 21:51:28 +0530

    initial project

---
+++ src/main/kotlin/org/harsh/gymmanagementbackend/GymManagementBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.harsh.gymmanagementbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GymManagementBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<GymManagementBackendApplication>(*args)
+}
